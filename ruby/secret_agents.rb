c0a10fa43a6fa9876216fda9023f6b500b1fd64a
Joe Viveiros <joe.viveiros@gmail.com>
2016-10-07 16:29:49 -0700

    Got the decrypt method working

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,7 +1,7 @@
 #encrypt method
 #Need to accept a string and move every letter of the string forward by one. 
  #Get user input
- # take user input and set to variable called Encrypt
+ # take user input and set to variable called encrypt
  # take each letter from the inputted string and advance (via .next) it one letter.
  # must figure out how to apply .next to every letter in the string and not just the final one.
 
@@ -15,3 +15,16 @@ def encrypt (y)
 	index +=1
 end
 end
+
+#decrypt method
+#Need to take the results of an entry and decrypt them basically reversing the advancement of one character to the next
+#My gut tells me that I'll have to create a variable with all of the letters and have them indexed so that 
+# I can pick against them using what the user input is. Meaning if the user input is r the method looksup where r is indexed and subtracts 1
+
+def decrypt (y)
+	alpha = "abcdefghijklmnopqrstuvwxyz"
+	replace = "zabcdefghijklmnopqrstuvwxy"
+	print y.tr(alpha, replace)
+	
+end	
+
