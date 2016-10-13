5a9104191563e383bd2b5de94d41728f9635a39f
Joe Viveiros <joe.viveiros@gmail.com>
2016-10-13 13:19:16 -0700

    Got nested data structure figured out and working

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,23 +2,38 @@
 
 city = {
 	roads: {
-		highways: [ 'Interstates', 'State Routes', 'Turnkpikes', "Connector Ramps"]
-		streets: ['Parkways', 'Main Thoroughfares', 'Residential Streets']
-		bikepaths: ['Paved Bike Path', 'Dirt Bike Paths', 'Hidden Bike Paths']
-	}
-	buildings: {
-		houses: ['Ranch Style Homes', 'Multi-story homes', 'mansions', 'cottages', 'shacks', 'apartments', 'lofts']
-		commercial: ['banks', 'office space', 'credit union', 'insurance agencies', 'tech center']
-		restaurants: [ 'fine dining', 'chain-restaurant', 'fast-food', 'sushi', 'mexican']
-		police_stations: ['prison', 'sherrif station', 'jail']
-
-	}
-	infrastructure: {
-		telephone:
+		highways: ['Interstates', 'State Routes', 'Turnkpikes', "Connector Ramps"],
+		bikepaths: 10,
+		streets: {
+			scenic:'Parkways', 
+			commuting:'Main Thoroughfares', 
+			slow_down:'Residential Streets'
+			},
 		
-	}
-	parks:
-	schools:
-	museums:
+		},
+	buildings: {
+		houses: ['Ranch Style Homes', 'Multi-story homes', 'mansions', 'cottages', 'shacks', 'apartments', 'lofts'],
+		commercial: ['banks', 'office space', 'credit union', 'insurance agencies', 'tech center'],
+		restaurants: [ 'fine dining', 'chain-restaurant', 'fast-food', 'sushi', 'mexican'],
+		police_stations: ['prison', 'sherrif station', 'jail'],
 
+	},
+	infrastructure: {
+		telephone: ['telephone poles', 'telephone lines', 'telephone sub-stations'],
+		electric: ['power lines', 'power poles', 'power substations', 'power generators'],
+		sewage: [ 'sewage drains', 'sewage pipes', 'sewage refinement', 'indoor plumbing'],
+		water: [ 'reservoirs', 'pipes', 'water treatment', 'indoor plumbing'],
+		gas: ['gas pipes', 'gas shut off valves', 'gas lines to house'],
+	},
+	parks: {
+		free: ['dog parks', 'skate parks', 'nature parks', 'childrens parks'],
+		paid: [ 'theme parks', 'water parks'],
+	},
+	schools: {
+		elementary: ['private', 'public', 'charter', 'religious affiliated'],
+		high_school: ['private', 'public', 'charter', 'religious affiliated', 'home-school'],
+		college: ['private', 'public', 'community college', 'state-schools', 'research university'],
+	}
 }
+
+
