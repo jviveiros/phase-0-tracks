2c5a51d4bcaeffbd4e59ce869dede23b1c607750
Joe Viveiros <joe.viveiros@gmail.com>
2016-10-13 12:38:46 -0700

    Began creating a complex nested table

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,24 @@
+#Building a nested data structure for a city
+
+city = {
+	roads: {
+		highways: [ 'Interstates', 'State Routes', 'Turnkpikes', "Connector Ramps"]
+		streets: ['Parkways', 'Main Thoroughfares', 'Residential Streets']
+		bikepaths: ['Paved Bike Path', 'Dirt Bike Paths', 'Hidden Bike Paths']
+	}
+	buildings: {
+		houses: ['Ranch Style Homes', 'Multi-story homes', 'mansions', 'cottages', 'shacks', 'apartments', 'lofts']
+		commercial: ['banks', 'office space', 'credit union', 'insurance agencies', 'tech center']
+		restaurants: [ 'fine dining', 'chain-restaurant', 'fast-food', 'sushi', 'mexican']
+		police_stations: ['prison', 'sherrif station', 'jail']
+
+	}
+	infrastructure: {
+		telephone:
+		
+	}
+	parks:
+	schools:
+	museums:
+
+}
