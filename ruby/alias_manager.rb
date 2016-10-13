bd0219fbaf96eef2fad9ed8549ee496ff86d6f2f
Joe Viveiros <joe.viveiros@gmail.com>
2016-10-13 14:49:27 -0700

    Got the method to go to the next character for the entire thing. Unable to work on vowels or deal with spaces yet

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,7 +4,32 @@
 #Step Two: change vowels to the next vowel, and move each consonant to the next one using a method (built-in)
 
 
-def alias (FullName)
-	fullname=.split(' ').reverse!
-	
+#Methods
+def name_reverse (fullname)
+	fullname.split(' ').reverse!.join(' ')
+end
+
+def name_encrypt (fullname)
+	name_array = fullname.split('')
+	name_array.each do |x|
+		if x == ["a", "e", "i", "o", "u" ]
+			x = vowels.next!
+		elsif x != ["a", "e", "i", "o", "u" ]
+			x.next!
+		else
+			x = " "
+		end
+	end
+	name_array.join('')
+		
+end
+
+
+
+
+
+
+
+
+
 
