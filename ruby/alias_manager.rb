8fa7a7f600258ba8186a2fd4b75c6bf4580f0c6a
Joe Viveiros <joe.viveiros@gmail.com>
2016-10-13 14:07:58 -0700

    Got the first part of swapping first and last name done

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,10 @@
+#Take a spy's inputted name from the user and do the following.
+ # first: swap the first and last name.
+     # makes sense to split the entry at the space and then create a new variable with the first and last names swapped
+#Step Two: change vowels to the next vowel, and move each consonant to the next one using a method (built-in)
+
+
+def alias (FullName)
+	fullname=.split(' ').reverse!
+	
+
