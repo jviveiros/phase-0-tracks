63b18b51d2bdfbbc91744bd654d7aed7cbabc076
Joe Viveiros <joe.viveiros@gmail.com>
2016-10-19 15:46:41 -0700

    Finished all release for Ruby 6.2. May need some refactoring or clarity gathering to show if we needed to create some sort of user input. '

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -33,3 +33,41 @@ Puppy.new.speak(10)
 Puppy.new.rollover
 Puppy.new.dog_years(10)
 Puppy.new.bark_backwards("bark")
+
+#This Class would be a calculator or Mathematician
+class Mathematician
+
+#Method to do addition
+	def addition(a, b)
+		puts a + b
+
+	end
+#Initialize Method that runs once we loop 50 times.
+	def initialize
+		puts "Performing calculation..."
+
+	end
+#Subtraction method
+	def subtraction(c, d)
+		puts c - d
+
+	end
+
+end
+
+#Array for storage of results from the instance of Mathematician Class
+results = []
+#Count to begin counting up to 50 loops
+count = 0
+
+until count > 50 
+	results << Mathematician.new #shovels each iteration into the results array.
+	count += 1
+end
+
+results.each do |math|
+	math.addition(3,4)
+    math.subtraction(9,5) 
+end
+
+p results
