c6286290185fd82bcf081cdcbd66ace5ca00faab
Joe Viveiros <joe.viveiros@gmail.com>
2016-10-19 12:52:26 -0700

    Finished Releases 0 and 1

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,31 @@ class Puppy
     toy
   end
 
+  def speak (times)
+
+  	puts "Woof! " * times
+  end
+
+  def rollover
+  	puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+  puts human_years * 7
+  end
+
+  def bark_backwards(bark)
+  	puts bark.reverse!
+  end	
+
+  def initialize
+  	puts "Initializing new puppy instance..."
+  end
+
 end
 
+Puppy.new.fetch ("ball")
+Puppy.new.speak(10)
+Puppy.new.rollover
+Puppy.new.dog_years(10)
+Puppy.new.bark_backwards("bark")
