d280079453beb624972ad54c2df4fd89d40bbd78
Joe Viveiros <joe.viveiros@gmail.com>
2016-10-20 14:56:32 -0700

    Stuck on Release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,11 +1,11 @@
 
 #Santa Class
 class Santa 
-	
+
 	@age = 0
 
 	def speak
-		puts "Ho, ho, ho! Haaaaappy holidays!"
+		puts "The #{@ethnicity} #{@gender} Santa says: Ho, ho, ho! Haaaaappy holidays!"
 	end
 	
 	def eat_milk_and_cookies (cookie)
@@ -18,37 +18,50 @@ class Santa
 		@ethnicity = ethnicity
 	end
 
-	def celebrate_birthday
-		@age += 1
+	def ethnicity
+		@ethnicity
 	end
 
-	def get_mad_at (reindeer)
-		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		reindeer_ranking.length.times do |i|
-			if i = reindeer
-				reindeer.index[8]
+	def gender
+		@gender
+	end
+
+	def age
+		@age
+	end
+
+	def celebrate_birthday
+		@age +=1
+	end
+
+	@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+
+	def get_mad_at(reindeer)	
+		@reindeer_ranking.each do |i|
+			if i = reindeer 
+				@reindeer_ranking = @reindeer_ranking.push(i)
 			end
 		end
+		p @reindeer_ranking
 	end
-
+	
 end
 
-santa = Santa.new("agender", "Latino")
-santa.eat_milk_and_cookies("Snickerdoodle")
-santa.get_mad_at("Blitzen")
 
+#Santa Holding Cell
+santas = []
 
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "unicorn", "N/A"]
 
-# #Santa Holding Cell
+example_genders.length.times do |i|
+	santas << Santa.new(example_genders[i], example_ethnicities[i])
+	puts "There are now #{santas.length} Santas in the array"
+end
 
-# santas = []
+santas.each do |santas|
+	santas.speak
+end
 
-# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "unicorn", "N/A"]
-
-# example_genders.length.times do |i|
-# 	santas << Santa.new(example_genders[i], example_ethnicities[i])
-# end
-
-# santas.each {|action| action.speak}
-# santas.each {|action| action.eat_milk_and_cookies("Chocolate Chip")}
+santa = Santa.new("Female", "black")
+santa.get_mad_at ("Rudolph")
