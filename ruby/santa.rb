9799f44fb6d862a64b61b6b6b8228d059fbd7f93
Joe Viveiros <joe.viveiros@gmail.com>
2016-10-20 13:20:11 -0700

    Finished Release 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,7 @@
-class Santa
-	def initialize
-		@gender = "Male"
-		@ethnicity = "Cuban"
-	end
 
-	reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+#Santa Class
+class Santa 
+	
 	@age = 0
 
 	def speak
@@ -15,8 +12,43 @@ class Santa
 		puts "That was a good #{cookie}"
 	end
 	
-	def initialize
+	def initialize (gender, ethnicity)
 		puts "Initializing Santa instance..."
-	end	
+		@gender = gender
+		@ethnicity = ethnicity
+	end
+
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at (reindeer)
+		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		reindeer_ranking.length.times do |i|
+			if i = reindeer
+				reindeer.index[8]
+			end
+		end
+	end
+
 end
 
+santa = Santa.new("agender", "Latino")
+santa.eat_milk_and_cookies("Snickerdoodle")
+santa.get_mad_at("Blitzen")
+
+
+
+# #Santa Holding Cell
+
+# santas = []
+
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "unicorn", "N/A"]
+
+# example_genders.length.times do |i|
+# 	santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+
+# santas.each {|action| action.speak}
+# santas.each {|action| action.eat_milk_and_cookies("Chocolate Chip")}
