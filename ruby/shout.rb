05488791ab7713573dc0dc809fddcc0da6d97b88
Joe Viveiros <joe.viveiros@gmail.com>
2016-10-22 15:12:10 -0700

    Finished Release 3 for shout.rb

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,41 @@
-module Shout
+# module Shout
  
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
   
-  def self.yell_happily(words) 
-  words + "!" + " :D"
-  end
+#   def self.yell_happily(words) 
+#   words + "!" + " :D"
+#   end
+
+# end
+
+module Shout
+
+	def yell_angrily(words)
+		words.upcase + "!!!" + " >:("
+	end
+
+	def yell_happily(words)
+		words.downcase + " <3" + "!"
+	end
 
 end
 
-p Shout.yell_angrily("Poopy Pants")
-p Shout.yell_happily("Poopy Pants")
+class Teacher
+include Shout
+end
+
+class Police_Officer
+include Shout
+end
+
+#Driver Code
+
+officer = Police_Officer.new
+puts officer.yell_angrily("stop right there")
+puts officer.yell_happily("I Love You")
+
+teacher = Teacher.new 
+puts teacher.yell_happily ("Great work children")
+puts teacher.yell_angrily ("Johnny put down the knife.")
