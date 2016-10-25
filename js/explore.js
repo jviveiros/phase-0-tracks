572c7d572e899d398d5638b501835d58b2f3fa51
Joe Viveiros <joe.viveiros@gmail.com>
2016-10-25 12:36:08 -0700

    Completed Release 4 by taking a string splitting into an array, reversing the array and then rejoining. I found this through javascript resources. It seems like a more elegant solution than doing a bubble sort that I was thinking of.

---
+++ js/explore.js
@@ -0,0 +1,18 @@
+// Pseudo-Code
+// Function must take a string and then re-organize the letters from right to left. 
+// The function should take the last letter and replace it with the first
+// then take the second to last letter and replace it with the second letter
+// and so on until it can no longer swap letters.
+
+function reverse(string) {
+	return string.split('').reverse().join('');
+}
+
+
+var output = reverse("string");
+
+if (1 == 1 && 2 == 2 ) {
+	console.log(output);
+} else {
+	console.log("You messed up the logic!");
+}
