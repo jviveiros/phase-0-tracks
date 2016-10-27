4cdc20529a8dcd7e307c3869ba67bc54453c79f0
Joe Viveiros <joe.viveiros@gmail.com>
2016-10-26 18:29:36 -0700

    Finished Release 2 & 3

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,4 +1,6 @@
 
+//Release 0
+
 var colors = ["blue", "black", "brown", "burnt sienna"];
 var horsenames = ["Ed", "Ethan", "Edgar","Eleanor"];
 
@@ -8,6 +10,7 @@ horsenames.push("Eduardo");
 console.log(colors);
 console.log(horsenames);
 
+//Release 1
 var horse = {};
 
 if (horsenames.length == colors.length){
@@ -17,3 +20,35 @@ if (horsenames.length == colors.length){
 } 
 
 console.log(horse);
+
+//Release 2
+
+function Car(make, model, year) {
+	console.log("Let's build a car:", this);
+
+	this.make = make;
+	this.model = model;
+	this.year = year;
+
+	this.rev = function() {console.log("Vrooom!"); };
+
+	console.log ("Your car has been built with the following specs:");
+}
+
+var car1 = new Car("Toyota", "Camry", "1988");
+console.log(car1);
+console.log("Our car can rev:");
+car1.rev();
+console.log("------")
+
+var car2 = new Car("Honda", "Accord", "1996");
+console.log(car2);
+console.log("Our car can rev:");
+car2.rev();
+console.log("------")
+
+var car3 = new Car("Ferrari", "Enzo", "2017");
+console.log(car3);
+console.log("Our car can rev:");
+car3.rev();
+console.log("------")
