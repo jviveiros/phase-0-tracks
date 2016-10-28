7075774389748b3c4b6be12b3e3cd3664170b4b2
Joe Viveiros <joe.viveiros@gmail.com>
2016-10-27 17:28:09 -0700

    Paused on Release 0. Got the algorithm to print each array item and it's length but having trouble getting the variable i created to continuously update it self so that it returns a false when something isn't longer than the last array item

--- js/algos.js
+++ js/algos.js
@@ -17,30 +17,20 @@ var testArray = ["A horse is a horse", "of course of course of course of course"
 function LongestPhrase(array) {
 	
 		for( var i = 0; i < array.length; i++){
-		  if (array[i].length > 0){
-		 	var lgth = array[i].length;
+		  var lgth =0
+		  if (array[i].length > lgth){
+		  	var lgth = array[i].length;
 		 	var longest = array[i];
-		} else {
-			longest = longest
-			lgth = lgth
-		}
+		} 
 
 		console.log(longest)
 		console.log(lgth)
-	}}
+}}	
 
 
-
-
-		//DOES NOT WORK	
-		// if (array[i].length > array.length[i+1]) {
-		// 	return array[i] = array[0];
-		// } else if (array[i+2].length > array.length[i+3]){
-		// 	return array[i+2] = array[0];
-		// } else if (array[i+3].length > array.length[i+4]){
-		// 	return array[i+3] = array[0]
-		// }else {
-		// 	return array[i+4] = array[0]
-		// }
-		
 var test1 = new LongestPhrase(testArray)
+
+//Release 1
+
+//Pseudo Code 
+//Function input needs to be an object or
